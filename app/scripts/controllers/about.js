ae702e8e241a44ccaf7e3d4c686df344e8fec5fd
ermanno <ermanno.pirotta@gmail.com>
2015-10-07 13:03:37 +0200

    project skeleton with angular i18n related components

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name servicefinderWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the servicefinderWebApp
+ */
+angular.module('servicefinderWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name servicefinderWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the servicefinderWebApp
+ */
+angular.module('servicefinderWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
